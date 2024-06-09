70d2cf73a7bfde1cafe589f82c74d9df25911039
2realzoo <realzoojin@gmail.com>
2024-06-09 17:39:36 +0900

    feat:add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./Main";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import MyPage from "./MyPage";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
