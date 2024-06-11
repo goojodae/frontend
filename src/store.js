7fdd93e96cb3cd6057e022b0524e41455df0f372
2realzoo <realzoojin@gmail.com>
2024-06-11 22:11:39 +0900

    feat:add logout

--- src/store.js
+++ src/store.js
@@ -4,8 +4,6 @@ const useStore = create(
     (set) => ({
         activeMenu: false,
         setActiveMenu: () => set((state) => ({ activeMenu: !state.activeMenu})),
-        isLogin:true,
-        setIsLogin: () => set((state) => ({ isLogin: !state.isLogin })),
         activeTooltip: false,
         setActiveTooltip: () => set((state) => ({ activeTooltip: !state.activeTooltip})),
     })
